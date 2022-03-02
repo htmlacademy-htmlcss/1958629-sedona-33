1a30989e747e098bcfa53a62157ab1ea6ccb44d8
VlasGor <gorbachevvlas@gmail.com>
2022-03-02 18:58:47 +0300

    обновленный сборщик и исправление мелких ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,99 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const nunjucks = require('nunjucks');
 const imagemin = require('gulp-imagemin');
 const server = require('browser-sync').create();
+const parser = require('posthtml-parser');
+const render = require('posthtml-render');
+const chalk = require('chalk');
+const fetch = require('node-fetch');
+const { HtmlValidate } = require('html-validate');
+
+const validateHtml = new HtmlValidate();
+const SeverityLevel = {
+	error: 2,
+	info: 1
+};
+const Severity = {
+  1: {
+    logOutput: chalk.yellow.bold,
+    title: 'WARNING'
+  },
+  2: {
+    logOutput: chalk.red.bold,
+    title: 'ERROR'
+  }
+};
+const isDev = process.env.NODE_ENV === 'development';
+const getPage = (tree) => tree.options.from.replace(/^.*source(\\+|\/+)(.*)\.html$/, '$2');
 
 const buildHTML = () => src(['source/**/*.html', '!source/**/_*.html'])
   .pipe(require('gulp-posthtml')([
     (() => (tree) => {
       nunjucks.configure('source', { autoescape: false });
 
-      return require('posthtml-parser')(nunjucks.renderString(require('posthtml-render')(tree), {}));
+      return parser(nunjucks.renderString(render(tree), {
+        isDev,
+        page: getPage(tree)
+      }));
+    })(),
+    (() => async (tree) => {
+      const html = render(tree);
+      const page = `${getPage(tree)}.html`;
+      let output = '';
+
+      try {
+        // Онлайн-валидатор HTML
+        const validRes = await fetch('https://validator.nu/?out=json', {
+          body: html,
+          headers: { 'Content-Type': 'text/html' },
+          method: 'POST'
+        });
+        const { messages = [] } = await validRes.json();
+        messages.forEach(({ extract, firstColumn, lastLine, message, type }) => {
+          const { logOutput, title } = Severity[SeverityLevel[type]];
+          const prefix = `\n[${chalk.cyan('Validate HTML Online')}] ${page} (${lastLine}:${firstColumn + 1})`;
+          const selectorMsg = ` ${chalk.cyan(extract)}`;
+
+          output += `${prefix}${selectorMsg}:\n${logOutput.underline(title)}: ${logOutput(message)}\n`;
+        });
+      } catch (err) {
+        // Оффлайн-валидатор HTML
+        const report = validateHtml.validateString(html, {
+          extends: [
+            'html-validate:recommended',
+            'html-validate:document'
+          ],
+          rules: {
+            'attribute-boolean-style': 'off',
+            'no-trailing-whitespace': 'off',
+            'input-missing-label': 'off',
+            'require-sri': 'off'
+          }
+        });
+        report.results.forEach(({ messages }) => {
+          messages.forEach(({ column, line, message, selector, severity }) => {
+            if (Severity[severity]) {
+              const { logOutput, title } = Severity[severity];
+              const prefix = `\n[${chalk.cyan('Validate HTML Offline')}] ${page} (${line}:${column})`;
+              const selectorMsg = selector ? ` ${chalk.cyan(selector)}` : '';
+
+              output += `${prefix}${selectorMsg}:\n${logOutput.underline(title)}: ${logOutput(message)}\n`;
+            }
+          });
+        });
+      } if (output) {
+				console.log(output);
+			}
+
+      return tree;
     })()
   ]))
-  .pipe(require('gulp-w3c-html-validator')())
   .pipe(require('gulp-html-beautify')())
   .pipe(dest('.'));
 
 const buildCSS = () => src(['source/**/*.css', '!source/**/_*.css'])
   .pipe(require('gulp-postcss')([
     require('postcss-easy-import')(),
-    require('stylelint')({ fix: true }),
+    require('stylelint')(),
     require('autoprefixer')(),
     require('postcss-reporter')({
       clearAllMessages: true,
@@ -27,33 +103,54 @@ const buildCSS = () => src(['source/**/*.css', '!source/**/_*.css'])
   ]))
   .pipe(dest('.'));
 
+const testBuildedCSS = () => src(['**/*.css', '!source/**/*.css', '!node_modules/**/*.css'])
+  .pipe(require('gulp-postcss')([
+    require('stylelint')({ fix: true }),
+    require('postcss-reporter')({
+      clearAllMessages: true,
+      throwError: false
+    })
+  ]));
+
 const optimizeImages = () => src('source/**/*.{svg,png,jpg}')
   .pipe(imagemin([
     imagemin.svgo({
       plugins: [
         {
-          name: 'removeViewBox',
-          active: false
+          removeViewBox: false
         },
         {
-          name: 'removeTitle',
-          active: true
+          removeTitle: true
         },
         {
-          name: 'cleanupNumericValues',
-          params: { floatPrecision: 2 }
+          cleanupNumericValues: {
+            floatPrecision: 2
+          }
         },
         {
-          name: 'convertPathData',
-          params: { floatPrecision: 2 }
+          cleanupNumericValues: {
+            floatPrecision: 2
+          }
         },
         {
-          name: 'convertTransform',
-          params: { floatPrecision: 2 }
+          convertPathData: {
+            floatPrecision: 2
+          }
         },
         {
-          name: 'cleanupListOfValues',
-          params: { floatPrecision: 2 }
+          transformsWithOnePath: {
+            floatPrecision: 2
+          }
+        },
+        {
+          convertTransform: {
+            floatPrecision: 2
+          }
+        },
+        {
+          cleanupListOfValues: {
+            floatPrecision: 2
+          }
         }
       ]
     }),
@@ -75,8 +172,9 @@ const startServer = () => {
   });
 
   watch('source/**/*.html', series(buildHTML, reload));
-  watch('source/**/*.css', series(buildCSS, reload));
+  watch('source/**/*.css', series(buildCSS, testBuildedCSS, reload));
   watch('source/**/*.{svg,png,jpg}', series(optimizeImages, reload));
 };
 
-exports.default = series(parallel(buildHTML, buildCSS, optimizeImages), startServer);
+exports.test = testBuildedCSS;
+exports.default = series(parallel(buildHTML, buildCSS, optimizeImages), testBuildedCSS, startServer);
